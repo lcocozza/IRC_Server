e894e2e2a409a69ecc3928b06d655d4291d0bc71
root <root@localhost.localdomain>
2018-12-30 16:51:06 +0100

    ?

--- server_app.c
+++ server_app.c
@@ -1,4 +1,3 @@
-
 #include "header.h"
 
 int	app(int master_socket, serv_config *s_conf)
@@ -9,6 +8,8 @@ int	app(int master_socket, serv_config *s_conf)
 	char buffer[1024];
 	int *clients_socket = NULL;
 	int statu = 0;
+	int fdmax;
+	int sd;
 	int i;
 
 	clients_socket = malloc(sizeof(int) * s_conf->max_client);
@@ -23,18 +24,28 @@ int	app(int master_socket, serv_config *s_conf)
 		FD_ZERO(&readfs);
 		FD_SET(STDIN_FILENO, &readfs);
 		FD_SET(master_socket, &readfs);
-		FD_SET(clients_socket[0], &readfs);
-		
-		if (select(master_socket + 1, &readfs, NULL, NULL, NULL) == -1)
+
+		fdmax = master_socket;
+
+		for (i = 0; i < s_conf->max_client; i++)
+		{
+			sd = clients_socket[i];
+			if (sd > 0)
+				FD_SET(sd, &readfs);
+			if (sd > fdmax)
+				fdmax = sd;
+		}
+
+		if (select(fdmax + 1, &readfs, NULL, NULL, NULL) == -1)
 		{
 			perror("select()");
 			exit(errno);
 		}
-		
+
 		if (FD_ISSET(STDIN_FILENO, &readfs))
 		{
 			fgets(buffer, sizeof(char) * 1024, stdin);
-			send_message(clients_socket[0], buffer);
+			send_toall(clients_socket, -1, s_conf->max_client, buffer);
 		}
 		else if (FD_ISSET(master_socket, &readfs))
 		{
@@ -53,6 +64,7 @@ int	app(int master_socket, serv_config *s_conf)
 				{
 					strcpy(buffer, "Connection error: no more client can be connected.\n");
 					send_message(new_socket, buffer);
+					cleanBuffer(buffer);
 					shutdown(new_socket, 2);
 					closesocket(new_socket);
 					break;
@@ -63,23 +75,31 @@ int	app(int master_socket, serv_config *s_conf)
 					printf("New client connected with socket %d from %s:%d, in slot %d\n", clients_socket[i], inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port), i);
 					strcpy(buffer, "Success connecting.\n");
 					send_message(clients_socket[i], buffer);
+					cleanBuffer(buffer);
 					break;
 				}
 			}
 		}
 		else
 		{
-			if (FD_ISSET(clients_socket[0], &readfs))
+			for (i = 0; i < s_conf->max_client; i++)
 			{
-				statu = receive_message(clients_socket[0], buffer);
-				if (statu == -1)
+				if (FD_ISSET(clients_socket[i], &readfs))
 				{
-					printf("Disconnect\n");
-				}
-				else
-				{
-					printf("%s", buffer);
-					viderBuffer();
+					statu = receive_message(clients_socket[i], buffer);
+					if (statu < 0)
+					{
+						printf("Disconnect\n");
+						shutdown(clients_socket[i], 2);
+						closesocket(clients_socket[i]);
+						clients_socket[i] = 0;
+
+					}
+					else
+					{
+						printf("%s\n", buffer);
+						cleanBuffer(buffer);
+					}
 				}
 			}
 		}
@@ -88,29 +108,47 @@ int	app(int master_socket, serv_config *s_conf)
 	return *clients_socket;
 }
 
+void	send_toall(int *clients_socket, int socket, int max, char *buffer)
+{
+	int i;
+
+	for (i = 0; clients_socket[i] < max; i++)
+	{
+		if (clients_socket[i] != socket)
+		{
+			printf("envois a socket = %d\n", clients_socket[i]);
+			send_message(clients_socket[i], buffer);
+		}
+	}
+	cleanBuffer(buffer);
+}
+
 void	send_message(SOCKET socket, char *buffer)
 {
+	int i;
+
+	for (i = 0; buffer[i] != '\n'; i++) ;
+	buffer[i] = '\0';
+
 	if (send(socket, buffer, strlen(buffer), 0) < 0)
 	{
 		perror("send()");
 		exit(errno);
 	}
-	viderBuffer();
 }
 
 int	receive_message(SOCKET socket, char *buffer)
 {
 	int statu = 0;
-	if (statu = recv(socket, buffer, 1024, 0) < 0)
+	if ((statu = recv(socket, buffer, 1024, 0)) < 0)
 		perror("recv()");
 	return statu;
 }
 
-void viderBuffer()
+void cleanBuffer(char *buffer)
 {
-    /*int c = 0;
-    while (c != '\n' && c != EOF)
-    {
-        c = getchar();
-    }*/
+	int i;
+	for (i = 1; buffer[i] != '\0'; i++)
+		buffer[i - 1] = '\0';
+	buffer[i] = '\0';
 }
